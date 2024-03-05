ed7f87ecef5b8a7b20deea176b92e36afe71034d
moses-ramoeletsi <mraomoeletsi@gmail.com>
2024-03-05 17:06:49 +0200

    added user follow function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,81 @@
+// import 'package:chat_app/pages/chat_page.dart';
+// import 'package:chat_app/services/auth/auth_services.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// class HomaPage extends StatefulWidget {
+//   const HomaPage({super.key});
+
+//   @override
+//   State<HomaPage> createState() => _HomaPageState();
+// }
+
+// class _HomaPageState extends State<HomaPage> {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   void signOut() {
+//     final authService = Provider.of<AuthService>(context, listen: false);
+//     authService.signOut();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home Page'),
+//         actions: [
+//           IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
+//         ],
+//       ),
+//       body: _buildUserList(),
+//     );
+//   }
+
+//   Widget _buildUserList() {
+//     return StreamBuilder<QuerySnapshot>(
+//       stream: FirebaseFirestore.instance.collection('users').snapshots(),
+//       builder: (context, snapshot) {
+//         if (snapshot.hasError) {
+//           return const Text('error');
+//         }
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Text('Loading...');
+//         }
+
+//         return ListView(
+//           children: snapshot.data!.docs
+//               .map<Widget>((doc) => _buildUserListItem(doc))
+//               .toList(),
+//         );
+//       },
+//     );
+//   }
+
+//   Widget _buildUserListItem(DocumentSnapshot docemunet) {
+//     Map<String, dynamic> data = docemunet.data()! as Map<String, dynamic>;
+
+//     if (_auth.currentUser!.email != data['email']) {
+//       return ListTile(
+//         title: Text(data ['email']),
+//         onTap: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => ChatPagge(
+//                   userReceiverEmail: data['email'],
+//                   userReceiverId: data['uid']),
+//             ),
+//           );
+//         },
+//       );
+//     } else {
+//       return Container();
+//     }
+//   }
+// }
+
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/services/auth/auth_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,7 +84,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomaPage extends StatefulWidget {
-  const HomaPage({super.key});
+  const HomaPage({Key? key});
 
   @override
   State<HomaPage> createState() => _HomaPageState();
@@ -14,6 +92,31 @@ class HomaPage extends StatefulWidget {
 
 class _HomaPageState extends State<HomaPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  Map<String, bool> followStatus = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize follow status when the widget is first initialized
+    _initializeFollowStatus();
+  }
+
+  void _initializeFollowStatus() {
+    FirebaseFirestore.instance
+        .collection('followers')
+        .doc(_auth.currentUser!.uid)
+        .snapshots()
+        .listen((snapshot) {
+      if (snapshot.exists) {
+        // Extract follow status from snapshot data
+        Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+        data.forEach((key, value) {
+          followStatus[key] = value;
+        });
+        setState(() {}); // Update UI
+      }
+    });
+  }
 
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
@@ -53,19 +156,33 @@ class _HomaPageState extends State<HomaPage> {
     );
   }
 
-  Widget _buildUserListItem(DocumentSnapshot docemunet) {
-    Map<String, dynamic> data = docemunet.data()! as Map<String, dynamic>;
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> userData = document.data()! as Map<String, dynamic>;
 
-    if (_auth.currentUser!.email != data['email']) {
+    if (_auth.currentUser!.email != userData['email']) {
+      bool isFollowing = followStatus.containsKey(userData['uid']) && followStatus[userData['uid']]!;
       return ListTile(
-        title: Text(data ['email']),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(userData['email']),
+            ElevatedButton(
+              onPressed: () {
+                // Implement follow/unfollow functionality here
+                _toggleFollow(userData['uid'], !isFollowing);
+              },
+              child: Text(isFollowing ? 'Following' : 'Follow'),
+            ),
+          ],
+        ),
         onTap: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => ChatPagge(
-                  userReceiverEmail: data['email'],
-                  userReceiverId: data['uid']),
+                userReceiverEmail: userData['email'],
+                userReceiverId: userData['uid'],
+              ),
             ),
           );
         },
@@ -73,5 +190,30 @@ class _HomaPageState extends State<HomaPage> {
     } else {
       return Container();
     }
+  }
+
+  void _toggleFollow(String userId, bool isFollowing) {
+    FirebaseFirestore.instance
+        .collection('followers')
+        .doc(_auth.currentUser!.uid)
+        .set({
+      userId: isFollowing,
+    }, SetOptions(merge: true)).then((value) {
+      // Update local state
+      setState(() {
+        followStatus[userId] = isFollowing;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(isFollowing ? 'You are now following this user.' : 'You have unfollowed this user.'),
+        ),
+      );
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to update follow status. Please try again later.'),
+        ),
+      );
+    });
   }
 }
