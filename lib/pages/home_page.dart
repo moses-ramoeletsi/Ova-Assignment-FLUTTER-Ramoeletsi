bb11e63136906b72cefd7ca1f8bf579cc8d34999
moses-ramoeletsi <mraomoeletsi@gmail.com>
2024-03-05 17:08:53 +0200

    added user follow function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,81 +1,3 @@
-// import 'package:chat_app/pages/chat_page.dart';
-// import 'package:chat_app/services/auth/auth_services.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-
-// class HomaPage extends StatefulWidget {
-//   const HomaPage({super.key});
-
-//   @override
-//   State<HomaPage> createState() => _HomaPageState();
-// }
-
-// class _HomaPageState extends State<HomaPage> {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-//   void signOut() {
-//     final authService = Provider.of<AuthService>(context, listen: false);
-//     authService.signOut();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Home Page'),
-//         actions: [
-//           IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
-//         ],
-//       ),
-//       body: _buildUserList(),
-//     );
-//   }
-
-//   Widget _buildUserList() {
-//     return StreamBuilder<QuerySnapshot>(
-//       stream: FirebaseFirestore.instance.collection('users').snapshots(),
-//       builder: (context, snapshot) {
-//         if (snapshot.hasError) {
-//           return const Text('error');
-//         }
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return const Text('Loading...');
-//         }
-
-//         return ListView(
-//           children: snapshot.data!.docs
-//               .map<Widget>((doc) => _buildUserListItem(doc))
-//               .toList(),
-//         );
-//       },
-//     );
-//   }
-
-//   Widget _buildUserListItem(DocumentSnapshot docemunet) {
-//     Map<String, dynamic> data = docemunet.data()! as Map<String, dynamic>;
-
-//     if (_auth.currentUser!.email != data['email']) {
-//       return ListTile(
-//         title: Text(data ['email']),
-//         onTap: () {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => ChatPagge(
-//                   userReceiverEmail: data['email'],
-//                   userReceiverId: data['uid']),
-//             ),
-//           );
-//         },
-//       );
-//     } else {
-//       return Container();
-//     }
-//   }
-// }
-
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/services/auth/auth_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -97,7 +19,6 @@ class _HomaPageState extends State<HomaPage> {
   @override
   void initState() {
     super.initState();
-    // Initialize follow status when the widget is first initialized
     _initializeFollowStatus();
   }
 
@@ -168,7 +89,6 @@ class _HomaPageState extends State<HomaPage> {
             Text(userData['email']),
             ElevatedButton(
               onPressed: () {
-                // Implement follow/unfollow functionality here
                 _toggleFollow(userData['uid'], !isFollowing);
               },
               child: Text(isFollowing ? 'Following' : 'Follow'),
@@ -199,7 +119,6 @@ class _HomaPageState extends State<HomaPage> {
         .set({
       userId: isFollowing,
     }, SetOptions(merge: true)).then((value) {
-      // Update local state
       setState(() {
         followStatus[userId] = isFollowing;
       });
